17563d0da088c794c257b7cc34094ca9f7e2e365
660853 <jj2497@comcast.net>
2020-10-20 16:09:36 +0000

    could not figure out how to make only one rect appear after making the ellipses mutli-color.

--- index.java
+++ index.java
@@ -5,36 +5,47 @@ int b= (int) random(1,255);
 //length and width rect
 int c= (int) random(50,150);
 //size of ellipse
-int z=12.5;
+int z= 12;
 //colors for ellipse
 int r= (int) random(1,255);
 int g= (int) random(1,255);
 int b= (int) random(1,255);
+
+
+
 void setup() {
 	size(500, 500);
   background(255);
-  }
-
-void draw() {
-//rect
+  
+  //rect
   stroke(0);
   fill(r,g,b);
   rect(a,b,c,c);
+ 
+  }
+
+void draw() {
+
 
 //ellipses
-  int r= (int) random(1,255);
-  int g= (int) random(1,255);
-  int b= (int) random(1,255);
+    r = (int) random(1,255);
+    g = (int) random(1,255);
+    b = (int) random(1,255);
+  stroke(0);
   fill(r,g,b);
+
   ellipse(mouseX+random(-40,40),mouseY-random(-40,40),z,z);
-
-  
-
-
-
+  fill((int) random(0, 255));
+  rect(a,b,c,c);
 }
 
   void mousePressed() {
-    fill(r,g,b);
-    rect(a,b,c,c);
+    r = (int) random(1,255);
+    g = (int) random(1,255);
+    b = (int) random(1,255);
+    background(255);
+    a = (int) random(2,240);
+    b = (int) random(1,255);
+    c = (int) random(50,150);
+    
   }
