d82b021743a1501e51350b8f753e79efbae13ef6
660853 <jj2497@comcast.net>
2020-10-20 02:26:20 +0000

    moving ellipse

--- index.java
+++ index.java
@@ -1,25 +1,27 @@
+int x=50;
+int y=50;
+int r=50;
+int q=50;
+int z=50;
 void setup() {
 	size(500, 500);
 }
 
 void draw() {
-// Step 1: Write code that draws the following screenshots 
-// with hard-coded values. (Feel free to use colors 
-// instead of grayscale.)
 
-// Step 2: Replace all of the hard-coded numbers with 
-// variables.
-
-// Step 3: Write assignment operations in draw() that 
-// change the value of the variables.
-// For example, “variable1 = variable1 + 2;”. 
-
-// Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
-
-  background(255);
+background(255);
   stroke(0);
 
+  fill(150);
+  rect(x,y,r+150,r+150);
+
+  fill(255);
+  ellipse(z+100,q+100,r,r);
+  q=q+1;
+  z=z+1
 
   
 
+
+// Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
 }
